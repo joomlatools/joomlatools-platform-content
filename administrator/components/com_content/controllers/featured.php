f7fd496fc17c8531cc7a0a27142e4214c08e3896
Allan Pilarca <allan@wizmedia.net>
2017-01-31 16:52:50 +0800

    #34: Apply joomla 3.6.5 changes in com:content

--- administrator/components/com_content/controllers/featured.php
+++ administrator/components/com_content/controllers/featured.php
@@ -3,22 +3,27 @@
  * @package     Joomla.Administrator
  * @subpackage  com_content
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-require_once __DIR__ . '/articles.php';
+JLoader::register('ContentControllerArticles', __DIR__ . '/articles.php');
 
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_content
+ * Featured content controller class.
+ *
+ * @since  1.6
  */
 class ContentControllerFeatured extends ContentControllerArticles
 {
 	/**
-	 * Removes an item
+	 * Removes an item.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
 	 */
 	public function delete()
 	{
@@ -31,7 +36,7 @@ class ContentControllerFeatured extends ContentControllerArticles
 		// Access checks.
 		foreach ($ids as $i => $id)
 		{
-			if (!$user->authorise('core.delete', 'com_content.article.'.(int) $id))
+			if (!$user->authorise('core.delete', 'com_content.article.' . (int) $id))
 			{
 				// Prune items that you can't delete.
 				unset($ids[$i]);
@@ -62,6 +67,7 @@ class ContentControllerFeatured extends ContentControllerArticles
 	 * Method to publish a list of articles.
 	 *
 	 * @return  void
+	 *
 	 * @since   1.0
 	 */
 	public function publish()
@@ -84,7 +90,6 @@ class ContentControllerFeatured extends ContentControllerArticles
 	 */
 	public function getModel($name = 'Feature', $prefix = 'ContentModel', $config = array('ignore_request' => true))
 	{
-		$model = parent::getModel($name, $prefix, $config);
-		return $model;
+		return parent::getModel($name, $prefix, $config);
 	}
 }
