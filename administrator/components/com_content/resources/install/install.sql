24d9a8a125500668eb29f0f8ad0689b96b880464
Allan Pilarca <allan@wizmedia.net>
2017-10-17 12:37:29 +0800

    #37: Set the asset_id of uncategorized category

--- administrator/components/com_content/resources/install/install.sql
+++ administrator/components/com_content/resources/install/install.sql
@@ -174,3 +174,9 @@ INSERT INTO `assets` (`parent_id`, `lft`, `rgt`, `level`, `name`, `title`, `rule
 VALUES
     ((SELECT `id` FROM (SELECT * FROM `assets`) AS `assets_table` WHERE `name` = 'com_content'), (SELECT `rgt` FROM (SELECT * FROM `assets`) AS `assets_table` WHERE `parent_id` = 1 ORDER BY `rgt` DESC LIMIT 1), (SELECT `rgt` + 1 FROM (SELECT * FROM `assets`) AS `assets_table` WHERE `parent_id` = 1 ORDER BY `rgt` DESC LIMIT 1), 2, 'com_content.category.2', 'Uncategorised', '{}');
 
+--
+-- Set the asset_id of Uncategorised category
+--
+
+UPDATE `categories` SET `asset_id` = (SELECT `id` FROM `assets` WHERE `name` = (SELECT CONCAT('com_content.category.', `id`) FROM (SELECT * FROM `categories`)  AS `categories_table` WHERE `categories_table`.`title` = 'Uncategorised' AND `categories_table`.`extension` = 'com_content')) WHERE `title` = 'Uncategorised' AND `extension` = 'com_content';
+
